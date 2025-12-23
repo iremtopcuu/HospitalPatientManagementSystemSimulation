0e30f630f8118cee4c220630270c745fb0414f9b
irem <iremtopcu69@gmail.com>
2025-12-23 17:35:04 +0300

    initial commit

--- src/TestHospitalSystem.java
+++ src/TestHospitalSystem.java
@@ -12,24 +12,28 @@ public class TestHospitalSystem {
         hs.addNewPatient(new Patient(90,"Tuğba ALkoç",5,19));
         hs.addNewPatient(new Patient(100,"Talha Mete Deveci",6,25));
 
-        hs.addTreatmentRequest(10,true); //tedavi taleplerini ekle priority=öncelikli true
+        hs.addTreatmentRequest(10,false); //tedavi taleplerini ekle priority=öncelikli true
         hs.addTreatmentRequest(50,false);
         hs.addTreatmentRequest(40,false);
         hs.addTreatmentRequest(60,true);
         hs.addTreatmentRequest(90,true);
         hs.addTreatmentRequest(80,false);
-        hs.addTreatmentRequest(70,false);
+        hs.addTreatmentRequest(70,true);
         hs.addTreatmentRequest(20,false);
 
-        hs.addDischargeRecord(new DischargeRecord(20,System.currentTimeMillis())); //taburcu kayıtları
+        hs.addDischargeRecord(new DischargeRecord(30,System.currentTimeMillis())); //taburcu kayıtları
         hs.addDischargeRecord(new DischargeRecord(80,System.currentTimeMillis()));
 
-        hs.processTreatment();// // process treatment requests (priority first)
+        hs.processTreatment();// // process treatment requests (priority first) Tedavi taleplerini işle (öncelikli olanlar önce)”
         hs.processTreatment();
         hs.processTreatment();;
         hs.processTreatment();
 
         hs.printSystem();
+
+        hs.sortBySeverity();
+        System.out.println("\n Patients sorted by their severity: ");
+        hs.patientList.printList();
 
 
 
